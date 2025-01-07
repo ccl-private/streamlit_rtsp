7a746a0eb1752e29985594a57dd1f9329c954dc5
ccl <36214675+ccl-private@users.noreply.github.com>
2025-01-07 14:02:56 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from st_rtsp_main import RTSPVideoStream
 def main():
 
     st.title("实时视频流")
-    rtsp_url = "rtsp://admin:zxkj@123@192.168.2.13:554/cam/realmonitor?channel=1&subtype=0"
+    rtsp_url = "rtsp://127.0.0.1:8554/123"
     video_stream = RTSPVideoStream(rtsp_url)
     video_stream.start_stream()
 
